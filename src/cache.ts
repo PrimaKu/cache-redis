7e002923c8f402bf0ba355af4dbc17b6a21c770d
Haris Thohir <haris@thohir.com>
2024-03-21 13:51:19 +0700

    fix: export module

---
+++ src/cache.ts
@@ -0,0 +1,45 @@
+import { RedisClientOptions, createClient } from 'redis';
+import { KEYS } from './constant';
+
+class Cache {
+  private client = createClient();
+
+  constructor(options: RedisClientOptions = {}) {
+    this.client = createClient(options);
+  }
+
+  async init() {
+    await this.client.on('error', (err) => console.error('Redis Client Error', err)).connect();
+  }
+
+  async get(key: string | KEYS): Promise<any | null> {
+    const value = await this.client.get(key);
+    let result = null;
+
+    try {
+      result = value ? JSON.parse(value) : null;
+    } catch (error) {
+      console.error(error);
+    }
+
+    return result ?? value;
+  }
+
+  async set(key: string | KEYS, value: any, expiry = 1440) {
+    let data;
+
+    try {
+      data = JSON.stringify(value);
+    } catch (error) {
+      console.error(error);
+    }
+
+    await this.client.set(key, data ?? value, { EX: expiry });
+  }
+
+  async del(key: string | KEYS) {
+    await this.client.del(key);
+  }
+}
+
+export default Cache;
